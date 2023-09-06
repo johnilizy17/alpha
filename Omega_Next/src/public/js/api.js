1933d812fcee12a8d8e801eec70ac11414ab00d8
Mini_Rogue <johnabrahamtosin@gmail.com>
2023-09-06 15:14:35 +0100

    about us

--- Omega_Next/src/public/js/api.js
+++
@@ -1,8 +0,0 @@
-
-async function fillTheTitle() {
-    const post = await fetch("https://omega.cyclic.app/soccer").then((res) => 
-    console.log(res)
-     
-    );
-  }
-  fillTheTitle();
--- Omega_Next/src/routes/token.js
+++
@@ -1,4 +0,0 @@
-export const NFT_Address = "0xB869be3e48C5b5C47F5d7d7Aa3CFc9DBd0D6FdD1"
-export const BUSD_Address = "0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814"
-export const BUSDPrice = 100
-export const BUSDPrice2 = 150 
